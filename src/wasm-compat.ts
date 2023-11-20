ba864dd074a9cb7ecfb42e41d79c1f74b20dfd32
Roman <iskullbreakeri@gmail.com>
2023-11-20 22:14:29 +0100

    Initial

---
+++ src/wasm-compat.ts
@@ -0,0 +1,3 @@
+const WASM_BASE64 = '';
+const wasm = /* #__PURE__ */ fetch('data:application/wasm;base64' + WASM_BASE64);
+export default wasm;
---
+++ src/wasm-default.ts
@@ -0,0 +1,2 @@
+const wasm = /* #__PURE__ */ fetch(/* #__PURE__ */ new URL('./suncalc.wasm', import.meta.url));
+export default wasm;
